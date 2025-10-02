3911fc5ee8afdf6acc400ac434b280d2638ae86d
RonaldoSigma228 <verty1993@bk.ru>
2025-10-02 20:41:00 +0300

    Hui

---
+++ FirstApi.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Сервер запущен на http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Ошибку запуска сервера: %v\n", err)
+	}
+}
